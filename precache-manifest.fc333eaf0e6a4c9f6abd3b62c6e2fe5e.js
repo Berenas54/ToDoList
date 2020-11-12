f71a68cd835a557bc44adcad39f19f0dc00e4448
Berenas54 <haravoy1998@mail.ru>
2020-11-12 23:47:33 +0300

    Updates

---
+++ precache-manifest.fc333eaf0e6a4c9f6abd3b62c6e2fe5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84254832c0ec611426fc9b5f3d8491f7",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "dbe99ec15b6374e4c53f",
+    "url": "/ToDoList/static/css/main.75193674.chunk.css"
+  },
+  {
+    "revision": "731265d97c0054bf0bf0",
+    "url": "/ToDoList/static/js/2.1d96c56f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.1d96c56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe99ec15b6374e4c53f",
+    "url": "/ToDoList/static/js/main.1cfb5640.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
