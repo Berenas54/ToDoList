258f3e1b99c615deae5d543d1ca6d4b6e4faca09
Berenas54 <haravoy1998@mail.ru>
2021-03-06 22:52:46 +0300

    task 16, add logout and login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useCallback, useEffect} from 'react'
 import './App.css'
-import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from '@material-ui/core'
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    IconButton,
+    LinearProgress,
+    Toolbar,
+    Typography
+} from '@material-ui/core'
 import {Menu} from '@material-ui/icons'
 import {TodolistsList} from '../features/TodolistsList/TodolistsList'
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from './store'
-import {RequestStatusType} from './app-reducer'
-import {BrowserRouter} from "react-router-dom";
+import {initializeAppTC, RequestStatusType} from './app-reducer'
+import {BrowserRouter, Route} from "react-router-dom";
 import {Login} from "../features/Login/Login";
+import {logoutTC} from '../features/Login/auth-reducer'
 
 type PropsType = {
     demo?: boolean
@@ -16,6 +26,25 @@ type PropsType = {
 
 function App({demo = false}: PropsType) {
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const initialized = useSelector<AppRootStateType, boolean>(state => state.app.initialized)
+    const isLoggedOn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [])
+
+
+    const logoutHandler = useCallback(() => {
+        dispatch(logoutTC())
+    }, [])
+
+
+    if (!initialized) {
+        return <div style={{position: "fixed", top: "30%", textAlign: "center", width: "100%"}}>{
+            <CircularProgress/>}</div>
+    }
+
     return (
         <BrowserRouter>
             <div className="App">
@@ -28,7 +57,7 @@ function App({demo = false}: PropsType) {
                         <Typography variant="h6">
                             News
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        {isLoggedOn && <Button color="inherit" onClick={logoutHandler}>Logout</Button>}
                     </Toolbar>
                     {status === 'loading' && <LinearProgress/>}
                 </AppBar>
---
+++ src/features/Login/auth-reducer.ts
@@ -0,0 +1,60 @@
+import {authAPI, LoginParamsType} from '../../api/todolists-api'
+import {Dispatch} from "redux";
+import {SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../../app/app-reducer'
+import {handleServerAppError, handleServerNetworkError} from "../../utils/error-utils";
+
+
+const initialState = {
+    isLoggedIn: false
+}
+
+export const authReducer = (state: initialStateType = initialState, action: ActionsType) => {
+    switch (action.type) {
+        case 'SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.value}
+        default:
+            return state
+    }
+}
+
+// actions
+export const setIsLoggedInAC = (value: boolean) => ({type: 'SET-IS-LOGGED-IN', value} as const)
+
+// thunks
+
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.login(data)
+        .then((res) => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(true))
+                dispatch(setAppStatusAC("succeeded"))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+export const logoutTC = () => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC("succeeded"))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}// types
+
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
+
+type initialStateType = {
+    isLoggedIn: boolean
+}
